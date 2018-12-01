d635faee4fde93d2909a9c42096364daa43269b5
ParcialWeb <00002517@uca.edu.sv>
2018-12-01 17:08:44 -0600

    añadiendo router.put de update

--- routes/filtro.js
+++ routes/filtro.js
@@ -4,4 +4,5 @@ const filtroController = require('../controllers/filtroController');
 
 router.get('/',filtroController.index);
 router.post('/',filtroController.store);
+router.put('/:id',filtroController.update);
 module.exports = router;
