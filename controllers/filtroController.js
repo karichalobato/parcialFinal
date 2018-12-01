1f5b54c325775efb49a2cac213f66d1460c5eb58
ParcialWeb <00002517@uca.edu.sv>
2018-12-01 15:21:17 -0600

    ingresando la columna 4 que hacia falta en models archivo filtro

--- controllers/filtroController.js
+++ controllers/filtroController.js
@@ -9,4 +9,20 @@ filtroController.index = async function (req,res,next) {
         return res.status(500).json({error:error});
     }
 }
+
+//insertar
+filtroController.store = store = async function(req,res,next){
+    let f = new filtro();
+    f.idCelular = req.body.idCelular;
+    f.marca = req.body.marca;
+    f.modelo = req.body.modelo;
+    f.bateria = req.body.bateria;
+    try{
+        await f.save();
+        return res.status(200).json({message:"INGRESO EXITOSO"});
+    }catch(error){
+        return res.status(500).json({error:error});
+    }
+}
+
 module.exports = filtroController;
--- models/filtro.js
+++ models/filtro.js
@@ -4,6 +4,7 @@ const {Schema} = mongoose;
 const filtroSchema = new Schema({
     idCelular:{type:String,unique:true,required:true},
     marca:{type:String,required:true},
-    modelo:{type:String,required:true}
+    modelo:{type:String,required:true},
+    bateria:{type:Number,required:true}
 });
 module.exports = mongoose.model('filtros',filtroSchema);
