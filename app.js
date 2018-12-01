94ba9fd6c4586f6b15d3d678edbb7bd447fee52c
ParcialWeb <00002517@uca.edu.sv>
2018-12-01 14:54:47 -0600

    abriendo conexion en archivo app.js

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var filtroRouter = require('./routes/filtro');
 
 var app = express();
+
+require('./configs/database'); //abriendo conexion
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/filtros',filtroRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
