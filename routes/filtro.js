ccee7540559d955eadd2f7afbe49b927c2b8e6aa
ParcialWeb <00002517@uca.edu.sv>
2018-12-01 15:24:03 -0600

    agregando router.post

--- routes/filtro.js
+++ routes/filtro.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const filtroController = require('../controllers/filtroController');
 
 router.get('/',filtroController.index);
+router.post('/',filtroController.store);
 module.exports = router;
